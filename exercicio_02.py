8ff486e5c4100ab197570505cae585e288623de3
Jeferson Almeida <jeferson.live11@gmail.com>
2025-02-28 15:34:44 -0300

    adicionando dicionario a uma lista

--- exercicio_02.py
+++ exercicio_02.py
@@ -14,7 +14,24 @@ livro: Dict[str, Any] = {
     
     }
 
-print(livro)
+        #print(livro)
+
+lista_de_livros_usando_dict:dict = {
+
+    "Livro 01" : {
+    "Título" : "Livro Python",
+    "Autor" : "Jeferson",
+    "Ano Publicado" : 2020},
+
+    "Livro 02" : {
+    "Título" : "Livro SQL",
+    "Autor" : "Almeida",
+    "Ano Publicado" : 2019}
+
+}
+
+print(lista_de_livros_usando_dict["Livro 02"]["Título"])
+
 
 #4. Escreva um programa que conta o número de ocorrências de cada caractere em uma string usando um dicionário.
 #5. Dada a lista `["maçã", "banana", "cereja"]` e o dicionário `{"maçã": 0.45, "banana": 0.30, "cereja": 0.65}`, calcule o preço total da lista de compras.
