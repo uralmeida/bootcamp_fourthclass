4b024cad10be76c3b87e63df939492c544e61312
Jeferson Almeida <jeferson.live11@gmail.com>
2025-02-27 22:08:26 -0300

    adicionei minha lista

---
+++ exemplo_lista.py
@@ -0,0 +1,14 @@
+
+
+produto_01: str = "Camiseta"
+produto_02: str = "Calça Jeans"
+produto_03: str = "Sapato"
+
+produtos: list = []
+
+produtos.append(produto_01)
+produtos.append(produto_02)
+produtos.append(produto_03)
+produtos.pop()
+
+print(produtos)
