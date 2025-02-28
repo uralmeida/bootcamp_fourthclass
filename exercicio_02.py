b18fad6c6dbc5672e3f0ef4cd34ef3bb18253ee7
Jeferson Almeida <jeferson.live11@gmail.com>
2025-02-28 15:59:29 -0300

    resolvendo exercicio 03

--- exercicio_02.py
+++ exercicio_02.py
@@ -34,4 +34,8 @@ print(lista_de_livros_usando_dict["Livro 02"]["Título"])
 
 
 #4. Escreva um programa que conta o número de ocorrências de cada caractere em uma string usando um dicionário.
+
+
+
+
 #5. Dada a lista `["maçã", "banana", "cereja"]` e o dicionário `{"maçã": 0.45, "banana": 0.30, "cereja": 0.65}`, calcule o preço total da lista de compras.
