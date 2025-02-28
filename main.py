d65aa2460a738dadffc9a235f18dc88fe4605754
Jeferson Almeida <jeferson.live11@gmail.com>
2025-02-27 21:51:12 -0300

    adicinei meu main

---
+++ main.py
@@ -0,0 +1,14 @@
+
+# Sem Type Hint
+
+idade = 30
+altura = 1.76
+nome = "Alice"
+is_studant = True
+
+# Com Type Hint
+
+idade: int = 30
+altura: float = 1.76
+nome: str = "Alice"
+is_studant: bool = True
