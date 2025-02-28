e35199df2bbea12afbe9015577d56c52e565cc62
Jeferson Almeida <jeferson.live11@gmail.com>
2025-02-27 22:22:33 -0300

    adicionei o exemplo de dicionario

---
+++ exemplo_dict.py
@@ -0,0 +1,31 @@
+
+
+produto_01 = {
+
+    "nome" : "televisão",
+    "quantidade" : 12,
+    "preço" : 279.12,
+    "disponibilidade" : True
+
+}
+ 
+produto_02 = {
+
+    "nome" : "notebook",
+    "quantidade" : 19,
+    "preço" : 321.80,
+    "disponibilidade" : False
+
+}
+
+produto_03 = {
+
+    "nome" : "fogão",
+    "quantidade" : 8,
+    "preço" : 42.15,
+    "disponibilidade" : True
+
+}
+
+produtos = [produto_01,produto_02,produto_03]
+print(produtos)
